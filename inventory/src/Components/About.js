abef08351ce5b2b422c8c8a3f2b9ad7712d8a369
Andy Lumsden <9976197+infinitejest69@users.noreply.github.com>
2022-02-05 17:31:14 +0000

    A second nested Component

---
+++ inventory/src/Components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+        <div>
+            <h1>Something About Me</h1>
+        </div>
+    );
+}
+
+ export default About;
--- inventory/src/Components/Info.js
+++ inventory/src/Components/Info.js
@@ -1,8 +1,10 @@
 import React from "react";
+import About from "./About";
 
 function Info(){
     return(
     <div>
+        <About />
         <p>Information Here</p>
     </div>
     );
